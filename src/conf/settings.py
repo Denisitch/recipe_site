ec0613d510537c92763f3f4260072abf6fb7e75f
Denisitch <denisitch@gmail.com>
2022-02-22 11:17:48 +0300

    Fixed db from docker

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -83,14 +83,7 @@ WSGI_APPLICATION = "conf.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "recipes_db",
-        "USER": "denisitch",
-        "PASSWORD": "05380730",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    "default": env.db(),
 }
 
 # Password validation
